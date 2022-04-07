d0b4fa48442672897a9cc0402423d7ec60799620
Julien <s.subrajulien@gmail.com>
2022-04-07 15:38:10 +0200

    xp formations

---
+++ src/datas/Experiences.js
@@ -0,0 +1,46 @@
+const dataExperiences = [
+    {
+        id:1,
+        title:"Intégrateur Développeur Web",
+        date:"16 Janvier 2020 - 25 Février 2022",
+        location: "Toulouse",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacinia blandit risus, et accumsan risus sagittis non.`,
+        missions: [
+            {
+                id: 1,
+                title: "mission 1",
+            },
+            {
+                id: 2,
+                title: "mission 1",
+            },
+            {
+                id: 3,
+                title: "mission 1",
+            },
+        ],
+    },
+    {
+        id:2,
+        title:"Stagiaire Intégrateur Web",
+        date:"16 Juillet 2019 - 16 Janvier 2020",
+        location: "Toulouse",
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacinia blandit risus, et accumsan risus sagittis non.`,
+        missions: [
+            {
+                id: 1,
+                title: "mission 1",
+            },
+            {
+                id: 2,
+                title: "mission 1",
+            },
+            {
+                id: 3,
+                title: "mission 1",
+            },
+        ],
+    },
+]
+
+export default dataExperiences
---
+++ src/datas/Formations.js
@@ -0,0 +1,16 @@
+const dataFormations = [
+    {
+        id:1,
+        title:"Formation Développeur Concepteur d'Applications Web",
+        date:"2020-2022",
+        location: "Toulouse",
+    },
+    {
+        id:2,
+        title:"Formation Développeur Intégrateur Web",
+        date:"2019",
+        location: "Toulouse",
+    },
+]
+
+export default dataFormations
